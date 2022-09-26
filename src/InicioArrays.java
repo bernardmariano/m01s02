a805e3203e8a053efb2d20c883ff099630e979bc
bernardmariano <bernard.vix@gmail.com>
2022-09-25 23:35:03 -0300

    Implementa exemplos de Array em Java

---
+++ src/InicioArrays.java
@@ -0,0 +1,16 @@
+public class InicioArrays {
+
+    public static void main(String[] args) {
+
+        int[] exemplo = {35, 33, 42, 10, 14, 19, 27, 44, 26, 31};
+
+        int[] exemplo2 = new int[5];
+        exemplo2[0] = 10;
+        exemplo2[1] = 6;
+        // ...
+        exemplo2[4] = 5; // ultimo indice
+
+        System.out.println( exemplo2[3] );
+
+    }
+}
